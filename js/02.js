f50d21f22e098a2a10b5082e8305d755086dc180
Xavier <tusaromix@gmail.com>
2021-01-19 19:06:07 +0100

    Inicio JS 19-01-2021

---
+++ js/02.js
@@ -0,0 +1,25 @@
+// Variables
+
+let producto = 'Audifonos Gamer'; // Iniciar variable y asignarle un valor
+
+let disponible; // Inicamos variable sin asignarle un valor
+
+// Reasignando el valor de la variable
+
+
+
+
+//Formas de declarar las variables 
+
+var producto1 = 'Computadora1',
+    disponible1 = true,
+    categoria = 'Computadoras';
+
+
+
+var nombre_producto = 'Monitor HD'; // underscore
+var nombreProducto = 'Monitor HD'; // Camelcase SINTAXIS MAS UTILIZADA
+var NombreProducto = 'Monitor HD'; // Pascal Case
+var nombreproducto = 'Monitor HD';
+
+console.log(producto); //Imprimimos por Consola la variable producto
---
+++ js/03.js
@@ -0,0 +1,27 @@
+// Variables
+
+const producto = 'Audifonos Gamer'; // Iniciar variable y asignarle un
+
+const disponible; // Inicamos variable sin asignarle un valor
+
+//CONST NO podemos inicializar una variable sin ningún valor
+
+
+
+
+
+
+//Formas de declarar las variables 
+
+var producto1 = 'Computadora1',
+    disponible1 = true,
+    categoria = 'Computadoras';
+
+
+
+var nombre_producto = 'Monitor HD'; // underscore
+var nombreProducto = 'Monitor HD'; // Camelcase SINTAXIS MAS UTILIZA
+var NombreProducto = 'Monitor HD'; // Pascal Case
+var nombreproducto = 'Monitor HD';
+
+console.log(producto); //Imprimimos por Consola la variable producto
