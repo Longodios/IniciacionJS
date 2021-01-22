e791b3416e755bb00ba7d99979ce21b230cf75b9
Xavier <tusaromix@gmail.com>
2021-01-22 19:07:04 +0100

    Version Viernes 22-01-2021

--- js/22.js
+++ js/22.js
@@ -1,9 +1,28 @@
 //Condicionales
 
-const puntaje = 1000;
+// const puntaje = 1000;
 
-if (puntaje == 1000) {
-    console.log("Si el puntaje es 1000");
+// if (puntaje == 1000) {
+// console.log("Si el puntaje es 1000");
+// } else {
+// console.log('No es igual');
+// }
+
+// const efectivo = 1000;
+// const carrito = 800;
+
+// if ( efectivo > carrito){
+// console.log('Usted puede pagar');
+// } else {
+// console.log('Fondo Insuficientes')
+// }
+
+const rol = 'editor';
+
+if (rol === 'administrador') {
+    console.log('Acceso a todo el sistema');
+} else if (rol === 'editor') {
+    console.log('Eres editor puedes entrar, pero no puedes hacer mucho');
 } else {
-    console.log('No es igual');
+    console.log('Gracias por visitar la página');
 }
---
+++ js/24.js
@@ -0,0 +1,12 @@
+// FOR LOOP
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 2 === 0) {
+        console.log(`El número ${i} es par `);
+    } else {
+        console.log(`El número ${i} es impar `);
+    }
+
+
+
+}
